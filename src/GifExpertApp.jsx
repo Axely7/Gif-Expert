1a11679824793eb3edddeb4232daee5533e16dde
Axely7 <axely7@hotmail.com>
2022-07-19 09:28:35 -0500

    Creating addcategory component

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import { AddCategory } from './components/AddCategory';
+
+export const GifExpertApp = () => {
+
+  const [categories, setCategories] = useState([ 'One Punch', 'Dragon Ball' ]);
+
+  const onAddCategory = () => {
+    setCategories([...categories, 'one piece'])
+  }
+
+
+  return (
+    <>
+      <h1>GifExpertApp</h1>
+      <AddCategory />
+      <button onClick={onAddCategory}>Agregar</button>
+      <ol>
+        { categories.map( category => {
+          return <li key={ category }>{ category }</li>
+        } ) }
+       
+      </ol>
+        
+    </>
+  )
+}
