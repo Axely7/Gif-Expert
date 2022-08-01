2e8b74fcbc2d764797de0bca89583bede5301410
Axely7 <axely7@hotmail.com>
2022-08-01 01:08:00 -0500

    creating cusomt hook

---
+++ src/hooks/useFetchGifs.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from "react";
+import { getGifs } from "../helpers/getGifs";
+
+export const useFetchGifs = (category) => {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const getImages = async () => {
+    const newImages = await getGifs(category);
+    setImages(newImages);
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    console.log("hola");
+    getImages();
+  }, []);
+  return {
+    images,
+    isLoading,
+  };
+};
